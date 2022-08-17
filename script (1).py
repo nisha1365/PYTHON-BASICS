2a13a7a83deb17acfe9551df660bf233c7cbded3
Nisha Choudhary <87658589+nisha1365@users.noreply.github.com>
2022-08-17 12:32:42 +0530

    Add files via upload

---
+++ script (1).py
@@ -0,0 +1,5 @@
+# To find minimum 
+a = input("Enter a: ")
+b = input("Enter b: ")
+c = min(a , b)
+print (c)
---
+++ script.py
@@ -0,0 +1,5 @@
+# To find maximum 
+a = input("Enter a: ")
+b = input("Enter b: ")
+c = max(a , b)
+print (c)
