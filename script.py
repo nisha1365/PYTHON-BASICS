57d78d9d18bcc5e826c9dc6729b9cb3abe955f1c
Nisha Choudhary <87658589+nisha1365@users.noreply.github.com>
2022-08-22 11:14:10 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
-# To find maximum 
-a = input("Enter a: ")
-b = input("Enter b: ")
-c = max(a , b)
-print (c)
+N = 10;
+#check for every number from 1 to N
+for i in range(1,N+1):
+  #check if current number is prime
+  if(isPrime(i)):
+    print(i,end=" ")
